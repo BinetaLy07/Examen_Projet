5d41e99f2c3d8aef75de4d8ba7236a712c27ad0d
Bineta Ly <lybineta712email@gmail.com>
2026-07-09 14:16:35 +0000

    premier commit

---
+++ src/main/java/sn/ipd/eduplus/examen_eduplus/ExamenEduplusApplication.java
@@ -0,0 +1,13 @@
+package sn.ipd.eduplus.examen_eduplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenEduplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenEduplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/ipd/eduplus/examen_eduplus/ExamenEduplusApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.ipd.eduplus.examen_eduplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenEduplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
